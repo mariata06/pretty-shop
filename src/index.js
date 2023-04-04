748e1be2236c6676a5f8f20bebc740b0f94ac2c3
mariata06 <slinkywizard@mail.ru>
2023-04-04 19:56:55 +0300

    Create global Filter Context

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ProductsProvider } from './context/products_context.js'
+import { ProductsProvider } from './context/products_context.js';
+import { FilterProvider } from './context/filter_context.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ProductsProvider>
-      <App />
+      <FilterProvider>
+        <App />
+      </FilterProvider>
     </ProductsProvider>
   </React.StrictMode>
 );
