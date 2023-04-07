c6f68c109e8a1000c1037ac6455ebb0793b1df59
mariata06 <slinkywizard@mail.ru>
2023-04-07 13:59:13 +0300

    Save cart items in LocalStorage, add basic cart template to Cart page

--- src/components/CartContent.js
+++ src/components/CartContent.js
@@ -1,12 +1,56 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+import { useCartContext } from '../context/cart_context';
+import { Link } from 'react-router-dom';
 
 const CartContent = () => {
+  const { cart } = useCartContext();
   return (
-    <div className="section section-center text-center content">
-        <h2>your cart here...</h2>
-    </div>
+    <Wrapper className="section section-center content">
+      <h2>your cart here...</h2>
+      <div>Cart Heading</div>
+      {
+        cart.map((item) => {
+          return <div key={item.id} {...item}>
+            <h5>Cart Item</h5>
+          </div>
+        })
+      }
+      <hr />
+      <div className='link-container'>
+        <Link to='/products' className='link-btn'>
+          continue shopping
+        </Link>
+        <button type='button' className='link-btn clear-btn'>
+          clear shopping cart
+        </button>
+      </div>
+      <div>Cart Total</div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.section`
+  .link-container {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 2rem;
+  }
+  .link-btn {
+    background: transparent;
+    border-color: transparent;
+    text-transform: capitalize;
+    padding: 0.25rem 0.5rem;
+    background: var(--color-medium-beige);
+    color: var(--color-white);
+    border-radius: var(--radius);
+    letter-spacing: var(--spacing);
+    font-weight: 400;
+    cursor: pointer;
+  }
+  .clear-btn {
+    background: var(--color-black);
+  }
+`
 
 export default CartContent;
--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -2,8 +2,17 @@ import React, { useEffect, useContext, useReducer } from 'react'
 import reducer from '../reducers/cart_reducer'
 import { ADD_TO_CART, REMOVE_CART_ITEM, TOGGLE_CART_ITEM_AMOUNT, CLEAR_CART, COUNT_CART_TOTALS } from '../actions'
 
+const getLocalStorage = () => {
+    let cart = localStorage.getItem('cart');
+    if(cart) {
+        return JSON.parse(localStorage.getItem('cart'))
+    } else {
+        return []
+    }
+}
+
 const initialState = {
-    cart: [],
+    cart: getLocalStorage(),
     total_items: 0,
     total_amount: 0,
     shipping_fee: 500,
@@ -18,6 +27,10 @@ export const CartProvider = ({ children }) => {
     const addToCart = (id, color, amount, product) => {
         dispatch({type: ADD_TO_CART, payload: {id, color, amount, product}})
     }
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(state.cart));
+    }, [state.cart])
 
     return (
         <CartContext.Provider value={{...state, addToCart}}>
